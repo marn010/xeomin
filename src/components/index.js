bb95b40cacf2588e4e3ebf8c24b42ae1f13af309
Miguel Rivera <migue.rivera@franca.agency>
2021-02-23 17:34:47 -0500

    Initial setup, added initial files

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component{
+   render(){
+      return(
+         <React.Fragment>
+            <p>This Page Works!</p>
+         </React.Fragment>
+      );
+   }
+}
