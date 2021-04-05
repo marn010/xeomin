4c132120ac1f50cc231a4c9876b26898a99ee010
Miguel Rivera <migue.rivera@franca.agency>
2021-04-05 10:56:01 -0500

    fixed visual issues in responsive social

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -8,17 +8,17 @@ export default class Social extends Component{
          <React.Fragment>
             <div className="row col-12 justify-content-center" style={{margin:"0%"}}>
                <div className="col-12 SocialBox">
-                  <div className="row col-12 justify-content-center">
-                     <div className="col-2 VisitBox font40">
+                  <div className="row col-12 " style={{padding:"0% 0% 0% 0.5%"}}>
+                     <div className=" VisitBox font40">
                         <p>VISITE</p>
                      </div>
-                     <div className="col-5 font24 instaBox">
+                     <div className=" font24 instaBox">
                         <a href="https://www.instagram.com/?hl=es" target="blank">
                            <img className="InstaBlack" src="/assets/img/Pics/InstaBlack.png" alt="instagram" />
                            <span>@merzaesthetics_col</span>
                         </a>
                      </div>
-                     <div className="col-5 font24 WebBox">
+                     <div className=" font24 WebBox">
                         <a href="https://www.merz.com/co" target="blank">
                            <img className="WebBlack" src="/assets/img/Pics/WebBlack.png" alt="Web" />
                            <span>www.merz.com/co</span>
